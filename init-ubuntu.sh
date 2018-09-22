c151dc960cffd5832d4d9d2c0146fa9a6326de7b
Fadi A <royalfadich@gmail.com>
2018-09-22 21:56:16 +0300

    Customization commands

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -32,3 +32,12 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
+
+
+# Customize Keys
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Super>"
+
+
+
+# Done.
+reboot
